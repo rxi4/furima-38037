e178ab10f036e71a8a5aed5ec1b4635adc7f8765
rxi4 <re093921@gmail.com>
2022-06-20 20:32:46 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_many :items
-  has_many :orders
+  #has_many :items
+  #has_many :orders
 
   with_options presence: true do
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
@@ -13,8 +13,8 @@ class User < ApplicationRecord
 
     validates :nickname
     validates :email,    uniqueness: true
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々ー]/ }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :birthday
